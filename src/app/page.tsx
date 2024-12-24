aa2784d79113a2b1ce02361c845fecf61b32846f
BenyounesMehdi <benyounesmahdi1234@gmail.com>
2024-12-24 20:22:59 +0000

    Added the landing page components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+import CTA from "@/components/landing/CTA";
+import Features from "@/components/landing/Features";
+import FrequentlyAskedQuestions from "@/components/landing/Frequently-Asked-Quetions";
+import Hero from "@/components/landing/Hero";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <>
+      <Hero />
+      <Features />
+      <FrequentlyAskedQuestions />
+      <CTA />
+    </>
+  );
 }
