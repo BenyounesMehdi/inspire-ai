2725bf635b882c37c949f6e8b7fa9c26a4db19e5
BenyounesMehdi <benyounesmahdi1234@gmail.com>
2024-12-24 21:35:32 +0000

    Added the Navbar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@ import CTA from "@/components/landing/CTA";
 import Features from "@/components/landing/Features";
 import FrequentlyAskedQuestions from "@/components/landing/Frequently-Asked-Quetions";
 import Hero from "@/components/landing/Hero";
+import Navbar from "@/components/navbar/Navbar";
 
 export default function Home() {
   return (
     <>
+      <Navbar />
       <Hero />
       <Features />
       <FrequentlyAskedQuestions />
