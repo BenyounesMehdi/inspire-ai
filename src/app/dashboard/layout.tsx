2d3de8c225a246b390e6fab4462d708a1798bc8a
BenyounesMehdi <benyounesmahdi1234@gmail.com>
2024-12-26 19:48:44 +0000

    Finihsed the routes setup

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,18 @@
+import { AppSidebar } from "@/components/dashboard/App-Sidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main>
+        <SidebarTrigger />
+        {children}
+      </main>
+    </SidebarProvider>
+  );
+}
