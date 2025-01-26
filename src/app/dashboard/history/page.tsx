153962a0e588ab06eb57488af847ca6dde454a58
BenyounesMehdi <benyounesmahdi1234@gmail.com>
2025-01-26 21:00:40 +0000

    Updated the ui

--- src/app/dashboard/history/page.tsx
+++ src/app/dashboard/history/page.tsx
@@ -1,3 +1,17 @@
+import History from "@/components/dashboard/History";
+
 export default function Page() {
-  return <div>History</div>;
+  return (
+    <div className="container w-full  px-5 py-8 space-y-8">
+      <div>
+        <h1 className="text-3xl font-bold tracking-tight">
+          Your Generation History
+        </h1>
+        <p className="text-lg text-muted-foreground mt-2">
+          View all your previously generated content
+        </p>
+      </div>
+      <History />
+    </div>
+  );
 }
