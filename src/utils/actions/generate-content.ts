14e89352297756951df5640d4f3b349baa21f09b
BenyounesMehdi <benyounesmahdi1234@gmail.com>
2025-01-28 20:33:13 +0000

    Added the user premuim verification

--- src/utils/actions/generate-content.ts
+++ src/utils/actions/generate-content.ts
@@ -4,6 +4,7 @@ import { State } from "@/types/Types";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { chatSession } from "../ai";
 import prisma from "@/lib/db";
+import { redirect } from "next/navigation";
 
 export async function generateContent(prevState: unknown, formData: FormData) {
   const { getUser } = getKindeServerSession();
@@ -11,42 +12,70 @@ export async function generateContent(prevState: unknown, formData: FormData) {
 
   if (!user) return null;
 
-  const userPrompt = formData.get("prompt");
-  const prePrompt = formData.get("prePrompt");
-  const prompt = prePrompt + "" + userPrompt;
-  const template = formData.get("template");
+  const [subscriptionStatus, content] = await Promise.all([
+    prisma.subscription.findUnique({
+      where: {
+        userId: user.id,
+      },
+      select: {
+        status: true,
+      },
+    }),
+    prisma.content.findMany({
+      where: {
+        userId: user.id,
+      },
+    }),
+  ]);
 
-  if (!userPrompt) {
-    const state: State = {
-      status: "error",
-      message: "Please set your prompt",
-    };
-    return state;
+  if (!subscriptionStatus || subscriptionStatus.status !== "active") {
+    if (content.length < 1) {
+      return createContent();
+    } else {
+      return redirect("/pricing");
+    }
+  } else if (subscriptionStatus.status === "active") {
+    return createContent();
   }
 
-  try {
-    const res = await chatSession.sendMessage(prompt);
-    const state: State = {
-      status: "success",
-      message: "valid",
-      content: res.response.text(),
-    };
+  async function createContent() {
+    const userPrompt = formData.get("prompt");
+    const prePrompt = formData.get("prePrompt");
+    const prompt = prePrompt + "" + userPrompt;
+    const template = formData.get("template");
 
-    await prisma.content.create({
-      data: {
-        userId: user.id,
-        template: template as string,
-        prompt: prompt,
-        output: res.response.text(),
-      },
-    });
+    if (!userPrompt) {
+      const state: State = {
+        status: "error",
+        message: "Please set your prompt",
+      };
+      return state;
+    }
 
-    return state;
-  } catch {
-    const state: State = {
-      status: "error",
-      message: "error",
-    };
-    return state;
+    try {
+      const res = await chatSession.sendMessage(prompt);
+      const state: State = {
+        status: "success",
+        message: "valid",
+        content: res.response.text(),
+      };
+
+      await prisma.content.create({
+        data: {
+          userId: user.id,
+          template: template as string,
+          prompt: prompt,
+          output: res.response.text(),
+        },
+      });
+
+      return state;
+    } catch {
+      const state: State = {
+        status: "error",
+        message: "error",
+      };
+      return state;
+    }
   }
 }
