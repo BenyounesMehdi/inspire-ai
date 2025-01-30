3fd0a9e46982b451f20ab9918080633eba19e8a0
BenyounesMehdi <benyounesmahdi1234@gmail.com>
2025-01-30 06:54:52 +0000

    refactor

--- src/app/dashboard/generate/page.tsx
+++ src/app/dashboard/generate/page.tsx
@@ -2,7 +2,8 @@ import TemplatesWrapper from "@/components/dashboard/TemplatesWrapper";
 import { getUserSubscription } from "@/utils/data/subscription/get-user-subscription";
 
 export default async function Page() {
-  const isSubscribedUser = await getUserSubscription();
+  const SubscribedUser = await getUserSubscription();
+  const isSubscribedUser = SubscribedUser?.status === "active";
 
   return <TemplatesWrapper isSubscribed={isSubscribedUser as boolean} />;
 }
