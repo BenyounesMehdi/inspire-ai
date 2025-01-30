e40097bc77aeab72b5d37a639697d8faffcc881f
BenyounesMehdi <benyounesmahdi1234@gmail.com>
2025-01-30 16:23:13 +0000

    Added the getUser function

--- src/app/dashboard/history/page.tsx
+++ src/app/dashboard/history/page.tsx
@@ -1,17 +1,17 @@
 import History from "@/components/dashboard/History";
+import { getUser } from "@/utils/data/user/get-user";
 
-export default function Page() {
+export default async function Page() {
+  const user = await getUser();
   return (
     <div className="container w-full  px-5 py-8 space-y-8">
       <div>
-        <h1 className="text-3xl font-bold tracking-tight">
-          Your Generation History
-        </h1>
+        <h1 className="text-3xl font-bold tracking-tight">Your History</h1>
         <p className="text-lg text-muted-foreground mt-2">
           View all your previously generated content
         </p>
       </div>
-      <History />
+      <History userId={user?.id as string} />
     </div>
   );
 }
