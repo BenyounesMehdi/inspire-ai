6f85378fe8b26a5423b045e8741dfb487435b38b
BenyounesMehdi <benyounesmahdi1234@gmail.com>
2025-02-01 16:02:37 +0000

    updated

--- src/utils/actions/create-subscription.ts
+++ src/utils/actions/create-subscription.ts
@@ -45,34 +45,30 @@ export const createSubscription = async (): Promise<void> => {
     });
   }
 
-  try {
-    const session = await stripe.checkout.sessions.create({
-      customer: stripeUserId.customerId as string,
-      mode: "subscription",
-      billing_address_collection: "auto",
-      payment_method_types: ["card"],
-      customer_update: {
-        address: "auto",
-        name: "auto",
+  const session = await stripe.checkout.sessions.create({
+    customer: stripeUserId.customerId as string,
+    mode: "subscription",
+    billing_address_collection: "auto",
+    payment_method_types: ["card"],
+    customer_update: {
+      address: "auto",
+      name: "auto",
+    },
+    line_items: [
+      {
+        price: process.env.STRIPE_PRICE_ID,
+        quantity: 1,
       },
-      line_items: [
-        {
-          price: process.env.STRIPE_PRICE_ID,
-          quantity: 1,
-        },
-      ],
-      success_url:
-        process.env.NODE_ENV === "production"
-          ? "https://inspire-ai-tech.vercel.app/payment/success"
-          : "http://localhost:3000/payment/success",
-      cancel_url:
-        process.env.NODE_ENV === "production"
-          ? "https://inspire-ai-tech.vercel.app/payment/cancelled"
-          : "http://localhost:3000/payment/cancelled",
-    });
-    return redirect(session.url as string);
-  } catch (error) {
-    console.error("Subscription creation error:", error);
-    throw error;
-  }
+    ],
+    success_url:
+      process.env.NODE_ENV === "production"
+        ? "https://inspire-ai-tech.vercel.app/payment/success"
+        : "http://localhost:3000/payment/success",
+    cancel_url:
+      process.env.NODE_ENV === "production"
+        ? "https://inspire-ai-tech.vercel.app/payment/cancelled"
+        : "http://localhost:3000/payment/cancelled",
+  });
+
+  return redirect(session.url as string);
 };
